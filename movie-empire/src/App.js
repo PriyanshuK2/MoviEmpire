49d66e8df204cf8c945b29c924200547976cced8
PriyanshuK2 <137306612+PriyanshuK2@users.noreply.github.com>
2025-08-16 21:25:46 +0530

    Update to App.js and App.css

--- movie-empire/src/App.js
+++ movie-empire/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
-
+import { useEffect } from 'react';
+// 8b3d536b
+const API_URL = 'http://www.omdbapi.com?apikey=8b3d536b';
 const App = () => {
+
+  const searchMovie = async(title) =>{
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+  }
+
+  useEffect(()=>{
+     searchMovie('Superman');
+  },[])
   return (
     <div className="App">
-     <h1>New App</h1>
+     <h1></h1>
     </div>
   );
 }
